4e8b1779bfeb43e1789b5141d8b5272f4acf1721
TAHIRY-TS <tahiryandriatefy82@gmail.com>
2025-06-04 20:59:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # utile en local, inoffensif sur Railway
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",") if x]
